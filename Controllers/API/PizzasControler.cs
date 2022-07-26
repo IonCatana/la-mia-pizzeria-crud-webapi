4b6e87a854f3c4f0576b694fe80d7909be4fb3c5
Ion Catana <catana.ion17@yahoo.it>
2022-07-26 15:18:41 +0200

    Add API Controller

---
+++ Controllers/API/PizzasControler.cs
@@ -0,0 +1,26 @@
+﻿using la_mia_pizzeria_model.Data;
+using la_mia_pizzeria_model.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_model.Controllers.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Pizze> pizze = new List<Pizze>();
+
+            using (PizzeContext db = new PizzeContext())
+            {
+                pizze = db.Pizze.ToList<Pizze>();
+            }
+
+            return Ok(pizze);
+        }
+
+    }
+}
